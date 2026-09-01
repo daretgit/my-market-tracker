434ce81eee611504371aaabdbccfe9f275b86e53
daretgit <2008cepeda.david@gmail.com>
2026-09-01 10:20:13 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -168,7 +168,7 @@ export default function Home() {
   const loginConGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: { redirectTo: window.location.origin },
+      options: { redirectTo: window.location.href },
     });
   };
 
